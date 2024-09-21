31dcfa4404291e98d49a1777612416978e16e7f6
FonsahPageo <ashprincepageo@gmail.com>
2024-09-21 22:01:52 +0100

    serving content form HTML

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-console.log("Welcome to Fonsah's restaurant");
-// alert("Welcome to Fonsah's restaurant");
-
 import "./styles.css";
+import restaurantImage from "./assets/restaurant.jpg";
+
+const imgElement = document.createElement("img");
+imgElement.src =  restaurantImage;
+document.getElementById('content').prependChild(imgElement);
+
+console.log("Welcome to Fonsah's restaurant");
