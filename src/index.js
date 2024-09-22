60a22e1f658e3125e123f0ca0d875dff4f7a91d6
FonsahPageo <ashprincepageo@gmail.com>
2024-09-22 07:04:50 +0100

    Dynamically loading content

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import "./styles.css";
 import restaurantImage from "./assets/restaurant.jpg";
 
+const headline = document.createElement("h3");
+headline.textContent = "Welcome to Fonsah's Restaurant where expectations meet reality";
+document.getElementById("content").appendChild(headline);
+
 const imgElement = document.createElement("img");
 imgElement.src =  restaurantImage;
-document.getElementById('content').prependChild(imgElement);
+document.getElementById("content").appendChild(imgElement);
+
+const description = document.createElement("p");
+description.textContent = "This is a description of the restaurant";
+document.getElementById("content").appendChild(description);
 
 console.log("Welcome to Fonsah's restaurant");
